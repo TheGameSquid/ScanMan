aa464c04f6c6784b06ca3b09c17b03e809e41291
TheGameSquid <robin.mattheussen@gmail.com>
2015-07-01 17:38:20 +0200

    Labels added

--- ScanMan/Controls/ModeKittingControl.Designer.cs
+++ ScanMan/Controls/ModeKittingControl.Designer.cs
@@ -33,6 +33,9 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.controlKittingAsset = new ScanMan.AssetKitting();
             this.buttonClear = new System.Windows.Forms.Button();
+            this.labelType = new System.Windows.Forms.Label();
+            this.labelAsset = new System.Windows.Forms.Label();
+            this.labelAD = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonPrint
@@ -84,10 +87,43 @@
             this.buttonClear.UseVisualStyleBackColor = true;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
+            // labelType
+            // 
+            this.labelType.AutoSize = true;
+            this.labelType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelType.Location = new System.Drawing.Point(190, 220);
+            this.labelType.Name = "labelType";
+            this.labelType.Size = new System.Drawing.Size(87, 37);
+            this.labelType.TabIndex = 32;
+            this.labelType.Text = "Type";
+            // 
+            // labelAsset
+            // 
+            this.labelAsset.AutoSize = true;
+            this.labelAsset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelAsset.Location = new System.Drawing.Point(411, 220);
+            this.labelAsset.Name = "labelAsset";
+            this.labelAsset.Size = new System.Drawing.Size(97, 37);
+            this.labelAsset.TabIndex = 33;
+            this.labelAsset.Text = "Asset";
+            // 
+            // labelAD
+            // 
+            this.labelAD.AutoSize = true;
+            this.labelAD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelAD.Location = new System.Drawing.Point(811, 220);
+            this.labelAD.Name = "labelAD";
+            this.labelAD.Size = new System.Drawing.Size(239, 37);
+            this.labelAD.TabIndex = 34;
+            this.labelAD.Text = "Active Directory";
+            // 
             // ModeKittingControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelAD);
+            this.Controls.Add(this.labelAsset);
+            this.Controls.Add(this.labelType);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.controlKittingAsset);
             this.Controls.Add(this.buttonPrint);
@@ -107,5 +143,8 @@
         private System.Windows.Forms.TextBox txtName;
         private AssetKitting controlKittingAsset;
         private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelType;
+        private System.Windows.Forms.Label labelAsset;
+        private System.Windows.Forms.Label labelAD;
     }
 }
